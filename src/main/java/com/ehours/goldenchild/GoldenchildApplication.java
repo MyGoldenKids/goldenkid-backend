1627b42d1a9076ab9a59f31d367a9de22ed0554b
kjy0349 <kjy0349@naver.com>
2024-01-17 10:35:58 +0900

    build: 스프링 초기세팅

---
+++ src/main/java/com/ehours/goldenchild/GoldenchildApplication.java
@@ -0,0 +1,12 @@
+package com.ehours.goldenchild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class GoldenchildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenchildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ehours/goldenchild/GoldenchildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehours.goldenchild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenchildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
