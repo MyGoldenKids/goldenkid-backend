2d857f39a8825ffa5ce5521ac320c750d8caf50f
kjy0349 <kjy0349@naver.com>
2024-01-18 11:22:11 +0900

    test: 게시판 수정 테스트

--- src/test/java/com/ehours/goldenchild/article/ArticleTests.java
+++ src/test/java/com/ehours/goldenchild/article/ArticleTests.java
@@ -78,4 +78,18 @@ public class ArticleTests {
         int retValue = articleService.articleDeleteRequest(articleReqDto.getArticleId());
         Assertions.assertThat(retValue).isEqualTo(1);
     }
+
+    @Test
+    @Transactional
+    void modifyTest() {
+        ArticleReqDto articleReqDto = ArticleReqDto.builder()
+                .memberId(4)
+                .fileListId(1)
+                .articleTitle("테스트용 게시글 제목")
+                .articleContent("테스트용 게시글 내용")
+                .build();
+        articleService.writeArticle(articleReqDto);
+        ArticleDetailDto articleDetailDto = articleService.getArticleDetailById(articleReqDto.getArticleId());
+
+    }
 }
