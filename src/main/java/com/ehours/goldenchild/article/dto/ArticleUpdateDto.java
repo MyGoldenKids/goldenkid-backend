4f340b5408188807b22996d385bdf99a3e003057
kjy0349 <kjy0349@naver.com>
2024-01-18 14:50:40 +0900

    fix: modify 기능이 제대로 동작하지 않는 경우 수정

--- src/main/java/com/ehours/goldenchild/article/dto/ArticleUpdateDto.java
+++ src/main/java/com/ehours/goldenchild/article/dto/ArticleUpdateDto.java
@@ -1,5 +1,10 @@
 package com.ehours.goldenchild.article.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ArticleUpdateDto {
     private int articleId;
     private int fileListId;
--- src/test/java/com/ehours/goldenchild/article/ArticleTests.java
+++ src/test/java/com/ehours/goldenchild/article/ArticleTests.java
@@ -3,6 +3,7 @@ package com.ehours.goldenchild.article;
 import com.ehours.goldenchild.article.dto.ArticleDetailDto;
 import com.ehours.goldenchild.article.dto.ArticleDto;
 import com.ehours.goldenchild.article.dto.ArticleReqDto;
+import com.ehours.goldenchild.article.dto.ArticleUpdateDto;
 import com.ehours.goldenchild.article.service.ArticleService;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -90,6 +91,14 @@ public class ArticleTests {
                 .build();
         articleService.writeArticle(articleReqDto);
         ArticleDetailDto articleDetailDto = articleService.getArticleDetailById(articleReqDto.getArticleId());
-
+        ArticleUpdateDto articleUpdateDto = new ArticleUpdateDto(articleDetailDto.getArticleId(),
+                articleDetailDto.getFileListId(),
+                "바뀐 제목",
+                "바뀐 내용");
+        articleService.updateArticle(articleUpdateDto);
+        ArticleDetailDto articleDetailDto1 = articleService.getArticleDetailById(articleDetailDto.getArticleId());
+        Assertions.assertThat(articleUpdateDto.getArticleTitle()).isEqualTo("바뀐 제목");
+        Assertions.assertThat(articleUpdateDto.getArticleContent()).isEqualTo("바뀐 내용");
+        log.info(articleUpdateDto.toString());
     }
 }
