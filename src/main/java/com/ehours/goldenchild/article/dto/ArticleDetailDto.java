17cbc61518369fb9a2d020bcc027ae389e87452a
kjy0349 <kjy0349@naver.com>
2024-01-18 11:24:40 +0900

    chore: 게시판 업데이트/상세보기 dto 빠진 것 추가

---
+++ src/main/java/com/ehours/goldenchild/article/dto/ArticleDetailDto.java
@@ -0,0 +1,16 @@
+package com.ehours.goldenchild.article.dto;
+
+import lombok.Data;
+
+@Data
+public class ArticleDetailDto {
+    private int articleId;
+    private int memberId;
+    private int fileListId;
+    private String articleTitle;
+    private String articleContent;
+    private String createdAt;
+    private String modifiedAt;
+    private int hit;
+    private int recommendCount;
+}
---
+++ src/main/java/com/ehours/goldenchild/article/dto/ArticleUpdateDto.java
@@ -0,0 +1,8 @@
+package com.ehours.goldenchild.article.dto;
+
+public class ArticleUpdateDto {
+    private int articleId;
+    private int fileListId;
+    private String articleTitle;
+    private String articleContent;
+}
