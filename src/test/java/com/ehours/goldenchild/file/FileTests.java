e9e543f0a02ffd02310c4591310cba7b12e9a137
kjy0349 <kjy0349@naver.com>
2024-01-19 14:43:18 +0900

    test: 파일 업로드 기능 테스트
    텍스트파일 2개, 동영상 파일 1개를 임시로 불러와 저장해보는 테스트

---
+++ src/test/java/com/ehours/goldenchild/file/FileTests.java
@@ -0,0 +1,58 @@
+package com.ehours.goldenchild.file;
+
+import com.ehours.goldenchild.file.service.FileService;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+@SpringBootTest
+public class FileTests {
+    @Autowired
+    FileService fileService;
+
+    private String getTestFilePath(String fileName) throws IOException, FileNotFoundException {
+        Path resourcePath = Paths.get(ResourceUtils.getURL("file:C:/Users/SSAFY/Downloads/").getPath());
+        return resourcePath.resolve(fileName).toString();
+    }
+
+    private MultipartFile createMockMultipartFile(String fileName) throws IOException {
+        String filePath = getTestFilePath(fileName);
+        byte[] content = Files.readAllBytes(Paths.get(filePath));
+        return new MockMultipartFile("file", fileName, "video/mp4", content);
+    }
+
+    @Test
+    void fileUploadTest() throws IOException {
+        MultipartFile multipartFile1 = new MockMultipartFile(
+                "file",
+                "test.txt",
+                "text/plain",
+                "test file".getBytes(StandardCharsets.UTF_8)
+        );
+        MultipartFile multipartFile2 = new MockMultipartFile(
+                "file",
+                "test.txt",
+                "text/plain",
+                "test file".getBytes(StandardCharsets.UTF_8)
+        );
+        MultipartFile multipartFile3 = createMockMultipartFile("SampleVideo_1280x720_1mb.mp4");
+        List<MultipartFile> list = new ArrayList<>();
+        list.add(multipartFile1);
+        list.add(multipartFile2);
+        list.add(multipartFile3);
+        int retValue = fileService.saveAllFiles(list, 4);
+        Assertions.assertThat(retValue).isEqualTo(3);
+    }
+}
