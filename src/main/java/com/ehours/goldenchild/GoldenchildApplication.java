eb262e2bc899e8ed4d73fbe6fb9a37131cf66ddc
kjy0349 <kjy0349@naver.com>
2024-01-23 15:29:19 +0900

    chore: CrossOrigin 테스트용

--- src/main/java/com/ehours/goldenchild/GoldenchildApplication.java
+++ src/main/java/com/ehours/goldenchild/GoldenchildApplication.java
@@ -2,7 +2,10 @@ package com.ehours.goldenchild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -10,5 +13,14 @@ public class GoldenchildApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoldenchildApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 }
