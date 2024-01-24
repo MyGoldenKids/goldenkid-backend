6149b472e8490b070c44cae039a4318d1aea94c6
dkdo1205 <dkdo1205@gmail.com>
2024-01-24 14:11:26 +0900

    refactor: 게시판 코드 개선 #28
    상세보기 페이지에 map으로 감싸져 있는 코드 개선
    중복 코드 개선

--- src/main/java/com/ehours/goldenchild/article/controller/ArticleController.java
+++ src/main/java/com/ehours/goldenchild/article/controller/ArticleController.java
@@ -5,6 +5,7 @@ import com.ehours.goldenchild.article.dto.ArticleDto;
 import com.ehours.goldenchild.article.dto.ArticleReqDto;
 import com.ehours.goldenchild.article.dto.ArticleUpdateDto;
 import com.ehours.goldenchild.article.service.ArticleService;
+import com.ehours.goldenchild.common.ResponseResource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,51 +30,35 @@ public class ArticleController {
     @PostMapping("/write")
     public ResponseEntity<Map<String, Object>> writeArticle(@RequestBody ArticleReqDto articleReqDto) {
         int retValue = articleService.writeArticle(articleReqDto);
-        if (retValue == 1) {
-            return handleSuccess(retValue, "게시판 글 등록 성공!");
-        } else return handleError("게시판 글 등록 실패..");
+        if (retValue == 1) return ResponseResource.handleSuccess(retValue, "게시판 글 등록 성공!");
+        else return ResponseResource.handleError("게시판 글 등록 실패..");
     }
 
     @GetMapping("/list")
     public ResponseEntity<Map<String, Object>> getAllArticles() {
         List<ArticleDetailDto> articleList = articleService.getAllArticles();
-        Map<String, Object> result = new HashMap<>();
-        result.put("data", articleList);
-        if (articleList != null) return handleSuccess(result, "게시판 리스트 조회 성공!");
-        else return handleError("게시판 리스트 조회 실패..");
+        if (articleList != null) return ResponseResource.handleSuccess(articleList, "게시판 리스트 조회 성공!");
+        else return ResponseResource.handleError("게시판 리스트 조회 실패..");
     }
 
     @PutMapping("/delete/{articleId}")
     public ResponseEntity<Map<String, Object>> articleDeleteRequest(@PathVariable int articleId) {
         int retValue = articleService.articleDeleteRequest(articleId);
-        if (retValue == 1) return handleSuccess(retValue, "삭제 요청 성공!");
-        else return handleError("삭제 요청 실패..");
+        if (retValue == 1) return ResponseResource.handleSuccess(retValue, "삭제 요청 성공!");
+        else return ResponseResource.handleError("삭제 요청 실패..");
     }
 
     @GetMapping("/detail/{articleId}")
     public ResponseEntity<Map<String, Object>> getArticleById(@PathVariable int articleId) {
         ArticleDetailDto articleDetailDto = articleService.getArticleDetailById(articleId);
-        if (articleDetailDto != null) return handleSuccess(articleDetailDto, "게시글 조회 성공");
-        else return handleError("게시글 조회 실패..");
+        if (articleDetailDto != null) return ResponseResource.handleSuccess(articleDetailDto, "게시글 조회 성공");
+        else return ResponseResource.handleError("게시글 조회 실패..");
     }
 
     @PutMapping("/modify")
     public ResponseEntity<Map<String, Object>> updateArticle(@RequestBody ArticleUpdateDto articleUpdateDto) {
         int retValue = articleService.updateArticle(articleUpdateDto);
-        if (retValue == 1) return handleSuccess(retValue, "게시글 수정 성공");
-        else return handleError("게시글 수정 실패..");
-    }
-
-    public ResponseEntity<Map<String, Object>> handleSuccess(Object data, String message) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("data", data);
-        map.put("message", message);
-        return ResponseEntity.status(HttpStatus.OK).body(map);
-    }
-
-    public ResponseEntity<Map<String, Object>> handleError(String message) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("message", message);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
+        if (retValue == 1) return ResponseResource.handleSuccess(retValue, "게시글 수정 성공");
+        else return ResponseResource.handleError("게시글 수정 실패..");
     }
 }
