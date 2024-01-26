f53799f48b864a6b80549f39873816c6ba2b6409
Hunnibs <cqqudgjs@hufs.ac.kr>
2024-01-26 11:15:36 +0900

    bug: http 모든 요청 허용 #37
    모든 요청이 권한 없이 이루어질 수 있도록 임시로 설정함

--- src/main/java/com/ehours/goldenchild/config/SecurityConfig.java
+++ src/main/java/com/ehours/goldenchild/config/SecurityConfig.java
@@ -2,13 +2,25 @@ package com.ehours.goldenchild.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .permitAll();
+
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
