7c37e67e777426a2eacc13d8315512e7a4f4fa5f
Hunnibs <106607029+Hunnibs@users.noreply.github.com>
2024-01-30 22:37:41 +0900

    feat: Security filter chain 기본 설정 #61
    cors와 authorizeHttpRequests 부분 정리
    rest api 사용이라 csrf는 disable로 설정

--- src/main/java/com/ehours/goldenchild/config/SecurityConfig.java
+++ src/main/java/com/ehours/goldenchild/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.ehours.goldenchild.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -14,11 +21,27 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests()
-                .anyRequest()
-                .permitAll()
-                .and()
-                .csrf().disable();
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/member/login", "/member/signup", "/member/idcheck/**",
+                                "/article/list", "/aritcle/detail/**", "comment/list/**")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .cors(httpSecurityCorsConfigurer -> new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+
+                        config.setAllowedOrigins(Collections.singletonList("*"));
+                        config.setAllowedMethods(Arrays.asList("GET","PUT","POST","DELETE"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return config;
+                    }
+                })
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
