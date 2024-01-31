54b1dd067849fc2cefc458b04ac79704b5af7b08
dkdo1205 <dkdo1205@gmail.com>
2024-01-31 10:23:43 +0900

    feat: 아이 조회 api 구현 #60
    delete mapping에 childId 추가

--- src/main/java/com/ehours/goldenchild/child/service/ChildService.java
+++ src/main/java/com/ehours/goldenchild/child/service/ChildService.java
@@ -3,10 +3,12 @@ package com.ehours.goldenchild.child.service;
 import com.ehours.goldenchild.child.dto.ChildDetailResDto;
 import com.ehours.goldenchild.child.dto.ChildModifyReqDto;
 import com.ehours.goldenchild.child.dto.ChildRegisterReqDto;
+import java.util.List;
 
 public interface ChildService {
     int registerChild(ChildRegisterReqDto childRegisterReqDto);
     ChildDetailResDto detailChild(int childId);
     int modifyChild(ChildModifyReqDto childModifyReqDto);
     int deleteChild(int childId);
+    List<ChildDetailResDto> getMyChild(int memberId);
 }
