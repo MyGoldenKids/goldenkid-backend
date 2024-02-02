4cbebdcbfbc0652a551e8f3134ad9ac1dfa4c25a
dkdo1205 <dkdo1205@gmail.com>
2024-02-02 15:46:37 +0900

    feat: 스토리 생성 구현 #54

---
+++ src/main/java/com/ehours/goldenchild/story/service/StoryService.java
@@ -0,0 +1,7 @@
+package com.ehours.goldenchild.story.service;
+
+import com.ehours.goldenchild.story.dto.StoryCreateReqDto;
+
+public interface StoryService {
+    int createStory(StoryCreateReqDto storyCreateReqDto);
+}
