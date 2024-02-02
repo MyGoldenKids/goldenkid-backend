92a980d527ad6747b4118e27b456f0a852732e04
dkdo1205 <dkdo1205@gmail.com>
2024-02-02 17:58:29 +0900

    feat: 스토리 상태 수정 구현 #54

--- src/main/java/com/ehours/goldenchild/story/service/StoryService.java
+++ src/main/java/com/ehours/goldenchild/story/service/StoryService.java
@@ -1,7 +1,9 @@
 package com.ehours.goldenchild.story.service;
 
 import com.ehours.goldenchild.story.dto.StoryCreateReqDto;
+import com.ehours.goldenchild.story.dto.StoryStatusReqDto;
 
 public interface StoryService {
     int createStory(StoryCreateReqDto storyCreateReqDto);
+    int updateStoryStatus(int storyId, StoryStatusReqDto storyStatusReqDto);
 }
