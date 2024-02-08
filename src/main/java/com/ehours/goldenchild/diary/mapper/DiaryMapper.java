2557145a69ae44ca4c29f9d24105bdc6189b0aec
dkdo1406 <54826394+dkdo1406@users.noreply.github.com>
2024-02-08 18:14:30 +0900

    fix: 다이어리 조회 문제 수정 #118

--- src/main/java/com/ehours/goldenchild/diary/mapper/DiaryMapper.java
+++ src/main/java/com/ehours/goldenchild/diary/mapper/DiaryMapper.java
@@ -1,5 +1,6 @@
 package com.ehours.goldenchild.diary.mapper;
 
+import com.ehours.goldenchild.article.dto.ArticleDetailDto;
 import com.ehours.goldenchild.diary.dto.DiaryCreateReqDto;
 import com.ehours.goldenchild.diary.dto.DiaryDateReqDto;
 import com.ehours.goldenchild.diary.dto.DiaryDetailResDto;
@@ -7,12 +8,8 @@ import com.ehours.goldenchild.diary.dto.DiaryResDto;
 import com.ehours.goldenchild.diary.dto.DiarySubmitReqDto;
 import com.ehours.goldenchild.diary.dto.DiaryUpdateReqDto;
 import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface DiaryMapper {
@@ -35,8 +32,8 @@ public interface DiaryMapper {
     int updateDiary(DiaryUpdateReqDto diaryUpdateReqDto);
 
     @Select("SELECT diary_id, diary_title, created_at from diary "
-            + "where member_id = #{memberId} and diary_status = 1 order by diary_id desc limit #{page} offset #{size}")
-    List<DiaryResDto> listDiary(int memberId, Integer page, Integer size);
+            + "where member_id = #{memberId} and diary_status = 1 order by diary_id desc limit #{size} offset #{page}")
+    List<DiaryResDto> listDiary(int memberId, Integer size, Integer page);
 
     @Select("SELECT d.diary_title, d.diary_content, d.diary_review, d.file_list_id, d.created_at, c.child_name "
             + "from diary d join child c on d.child_id = c.child_id "
